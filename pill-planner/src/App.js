15f7696098c1998764425303fff79a5742c041bb
CharanSuggala26 <suggalasaicharan789@gmail.com>
2024-12-24 14:32:35 +0530

    Tailwind has been setup

--- pill-planner/src/App.js
+++ pill-planner/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Components/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to PillPlanner</h1>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
