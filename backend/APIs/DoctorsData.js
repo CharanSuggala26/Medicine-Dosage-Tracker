fc298f37bcd328db96918d619220582a3d754a14
pranay65 <family.yerram@gmail.com>
2024-12-30 22:16:33 +0530

    Added DoctorsData API

---
+++ backend/APIs/DoctorsData.js
@@ -0,0 +1,14 @@
+const exp = require("express");
+const expAsyncHandler = require("express-async-handler");
+const doctorsApp = exp.Router();
+
+const fetchAllDoctors = expAsyncHandler(async (req, res) => {
+  const doctorsCollection = req.app.get("doctorsCollection");
+  let resData = await doctorsCollection.find().toArray();
+  console.log(resData);
+  return res.send({ status: 200, payload: resData });
+});
+
+doctorsApp.get("/", fetchAllDoctors);
+
+module.exports = doctorsApp;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
 require("dotenv").config();
 const exp = require("express");
+const cors = require("cors");
+const doctorsApp = require("./APIs/DoctorsData");
+const client = require("mongodb").MongoClient;
 const app = exp();
 const port = process.env.PORT || 3500;
+app.use(cors());
+app.use(exp.json());
+
+client
+  .connect(process.env.DB_URI)
+  .then((client) => {
+    const dbObj = client.db("medicineDB");
+    const doctorsCollection = dbObj.collection("doctors");
+    app.set("doctorsCollection", doctorsCollection);
+    console.log("DB Connected Successfully.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.send({ message: "Test for CORS." });
+});
+
+app.use("/doctors", doctorsApp);
 
 app.listen(port, () => {
   console.log(`Server running on ${port}.`);
